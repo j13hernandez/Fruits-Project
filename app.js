930ca38febcb1fce3d76cf92d86b94a95d75afb9
Palma <1hernandezj3@gmail.com>
2022-01-04 14:44:47 -0500

    "Created a fruit and people database and added fruits and people to it and linked fruits to people using mongoose"

--- app.js
+++ app.js
@@ -1,41 +1,80 @@
-const { MongoClient } = require('mongodb');
+const mongoose = require('mongoose');
 // Replace the uri string with your MongoDB deployment's connection string.
-const uri = 'mongodb://localhost:27017';
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    await client.connect();
-    const database = client.db('fruitsDB');
-    const fruits = database.collection('fruits');
-    // create a document to insert
-    const doc = [
-      {
-        name: 'Blueberry',
-        score: 9,
-        review: 'Great blueberries!',
-      },
-      {
-        name: 'Orange',
-        score: 8,
-        review: 'Enjoyable, but I wish there were more.',
-      },
-      {
-        name: 'Apple',
-        score: 5,
-        review: 'Apples are ok.',
-      },
-      {
-        name: 'raisins',
-        score: 3,
-        review: 'EWW!',
-      },
-    ];
-    // insert the document into the collection
-    const result = await fruits.insertMany(doc);
-    console.log(`${result.insertedCount} document was inserted.`);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+const uri = 'mongodb://localhost:27017/fruitsDB';
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const fruitsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  rating: Number,
+  review: String,
+});
+const personsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  age: Number,
+  favoriteFruit: fruitsSchema,
+});
+
+const Person = mongoose.model('Person', personsSchema);
+
+const Fruit = mongoose.model('Fruit', fruitsSchema);
+
+const pineapple = new Fruit({
+  name: 'Pineapple',
+  rating: 10,
+  review: 'Great fruit',
+});
+
+pineapple.save();
+
+const person1 = new Person({
+  name: 'John Dow',
+  age: 25,
+  favoriteFruit: pineapple,
+});
+
+person1.save();
+
+// const person2 = new Person({
+//   name: 'Johnathan Doe',
+//   age: 15,
+// });
+
+// const person3 = new Person({
+//   name: 'Joan Doe',
+//   age: 45,
+// });
+
+// Person.insertMany([person1, person2, person3]).then(
+//   () => {
+//     console.log('People saved successfully');
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+// Person.deleteOne({ name: 'John Doe' }).then(
+//   () => {
+//     console.log('Person deleted successfully');
+//   },
+//   (err) => {
+//     console.log(err);
+//   }
+// );
+
+Person.find()
+  .then((persons) => {
+    persons.forEach((person) => {
+      console.log(person.name);
+    });
+    // mongoose.disconnect();
+  })
+  .catch((err) => {
+    console.log(err);
+  });
